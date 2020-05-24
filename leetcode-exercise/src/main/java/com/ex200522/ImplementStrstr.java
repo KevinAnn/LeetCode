75189e52150e5e970a1eaeab594484a131b56cf6
qi.an <qi.an01@ucarinc.com>
2020-05-25 07:52:21 +0800

    200522

---
+++ leetcode-exercise/src/main/java/com/ex200522/ImplementStrstr.java
@@ -0,0 +1,42 @@
+package com.ex200522;
+
+import com.util.Util;
+
+/**
+ * 28. 实现 strStr()
+ *
+ * 实现 strStr() 函数。
+ * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+ * 示例 1:
+ * 输入: haystack = "hello", needle = "ll"
+ * 输出: 2
+ * 示例 2:
+ * 输入: haystack = "aaaaa", needle = "bba"
+ * 输出: -1
+ *
+ * @author An Qi
+ * @date 2020/5/22
+ */
+public class ImplementStrstr {
+
+    public static void main(String[] args) {
+        String haystack = "";
+        String needle = "";
+        Util.print(strStr(haystack, needle));
+    }
+
+    /**
+     * 子串检索
+     */
+    private static int strStr(String haystack, String needle) {
+        for (int i = 0; i < haystack.length() + 1 - needle.length(); i++) {
+            if (haystack.startsWith(needle, i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+
+}
---
+++ leetcode-exercise/src/main/java/com/ex200522/RepeatedSubstringPattern.java
@@ -0,0 +1,41 @@
+package com.ex200522;
+
+import com.util.Util;
+
+/**
+ * 459. 重复的子字符串
+ *
+ * 给定一个非空的字符串，判断它是否可以由它的一个子串重复多次构成。给定的字符串只含有小写英文字母，并且长度不超过10000。
+ * 示例 1:
+ * 输入: "abab"
+ * 输出: True
+ * 解释: 可由子字符串 "ab" 重复两次构成。
+ *
+ * @author An Qi
+ * @date 2020/5/22
+ */
+public class RepeatedSubstringPattern {
+
+    public static void main(String[] args) {
+        String s = "";
+        Util.print(repeatedSubstringPattern(s));
+    }
+
+    /**
+     * 本题关键点在于寻找判断是否由重复子串构成的特征，
+     * 若由重复子串构成，则s移位<n次后仍旧和变成s，
+     * 即：
+     * abcabc
+     * 转变:cabcab
+     * 改变两次:bcabca
+     * 改变三次:abcabc
+     * 由于在s很长的情况下，完成移动操作效率很低，
+     * 因此使s2=s+s，当移动到第二个s之前时出现相同串则表示含有重复子串
+     */
+    private static boolean repeatedSubstringPattern(String s) {
+        if (s.length() < 2) {
+            return false;
+        }
+        return (s + s).substring(1, s.length() * 2 - 1).contains(s);
+    }
+}
